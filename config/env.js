63e7b35f62c0303ffa30b7b6857c2075d81a5c31
Drey22230 <andriexenon@gmail.com>
2026-01-29 01:05:40 +0800

    backend  ready

--- config/env.js
+++ config/env.js
@@ -5,10 +5,9 @@ export const config = {
   port: process.env.PORT || 3000, 
 
   db: {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE__URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 };
